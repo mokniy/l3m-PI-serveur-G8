7e21a7aae298c985e43fc41a54d85481e6ece644
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-04-11 19:53:51 +0200

    amorce

---
+++ src/main/java/com/example/RestServer.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServer {
+    public static void main(String[] args) {
+        SpringApplication.run(RestServer.class, args);
+    }
+
+}
